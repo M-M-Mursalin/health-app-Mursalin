4b4eda2da85a1d35e51bcc03bc61eb0afd635348
M-M-Mursalin <69346389+M-M-Mursalin@users.noreply.github.com>
2024-12-17 20:33:36 +0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,29 @@
+// let countER = document.querySelector('.counter');
+// let count = 1;
+// setInterval(() =>{
+//     if(count < 75){
+//         count++;
+//         countER.innerText = count;
+//     }
+// }, 10);
+
+let counters = document.querySelectorAll('.counter');
+let time = 300;
+counters.forEach(counter => {
+    let updateCount = () => {
+    let target = +counter.getAttribute('data-target');
+    let count = +counter.innerText;
+    let increament = target/time;
+    if(count < target){
+        counter.innerText = Math.ceil(count + increament);
+        setTimeout(updateCount, 1);
+    }else{
+        counter.innerText = target;
+    }
+    };
+    updateCount();
+})
+let healthCondition = document.querySelector('.health');
+setTimeout(() =>{
+    healthCondition.innerText = 'You are FIT! but মন খারাপ 🥹'
+}, 3000);
